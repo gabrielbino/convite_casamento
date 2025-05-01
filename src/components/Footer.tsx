1a8d0fddfb688293cae984c53d5e756b7da8b260
gabrielbino <gabriellf650@gmail.com>
2025-05-01 17:29:01 -0300

    adicionar links das redes sociais e imagem ao compartilhar o link do site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,16 +8,16 @@ export default function Footer() {
         <p className="font-medium">Desenvolvido por</p>
         <p className="font-semibold">Gabriel Bino e Gilberto Bino</p>
         <div className="flex justify-center gap-6 text-[#426221]">
-          <a href="https://instagram.com/seu_instagram_gabriel" target="_blank" rel="noopener noreferrer" aria-label="Instagram Gabriel">
+          <a href="https://www.instagram.com/gabrielfbino?igsh=bjQ0b3FiamVuZnk5&utm_source=qr" target="_blank" rel="noopener noreferrer" aria-label="Instagram Gabriel">
             <FaInstagram size={18} />
           </a>
-          <a href="https://linkedin.com/in/seu_linkedin_gabriel" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Gabriel">
+          <a href="https://www.linkedin.com/in/gabrielfbino?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Gabriel">
             <FaLinkedin size={18} />
           </a>
-          <a href="https://instagram.com/seu_instagram_gilberto" target="_blank" rel="noopener noreferrer" aria-label="Instagram Gilberto">
+          <a href="https://www.instagram.com/gilbertobarcelosb?igsh=YTI5YXJ3ZmdzZnR0&utm_source=qr" target="_blank" rel="noopener noreferrer" aria-label="Instagram Gilberto">
             <FaInstagram size={18} />
           </a>
-          <a href="https://linkedin.com/in/seu_linkedin_gilberto" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Gilberto">
+          <a href="https://www.linkedin.com/in/gilberto-barcelos-bino-995574155?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Gilberto">
             <FaLinkedin size={18} />
           </a>
         </div>
